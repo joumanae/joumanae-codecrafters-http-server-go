bccdd13289bf902280ea431623556b36ea33029b
Joumana El Alaoui <joumana.codes@gmail.com>
2024-07-31 15:28:24 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
@@ -14,16 +11,18 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
 }
