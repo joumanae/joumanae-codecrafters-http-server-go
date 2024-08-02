caa25a2042256df20b1ed6edff5e560c5e7d0984
Joumana El Alaoui <joumana.codes@gmail.com>
2024-08-02 12:50:02 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -17,21 +17,28 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	// In this stage, your server will extract the URL path from an HTTP request, and respond with either a 200 or 404,
-	//depending on the path.
-
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	req := make([]byte, 1024)
-	conn.Read(req)
-
-	if !strings.Contains(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	request := string(buffer[:n])
+	path := strings.Split(request, " ")[1]
+
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		body := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(body), body)))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	conn.Close()
 }
