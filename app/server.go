37ed417a43bc855f9d810f4c52e4268d4c7eedb5
Joumana El Alaoui <joumana.codes@gmail.com>
2024-09-09 15:27:31 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,45 +9,57 @@ import (
 	"strings"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	request, err := http.ReadRequest(reader)
+	if err != nil {
+		fmt.Println("Error reading request:", err)
+		return
+	}
+
+	// Write the request to stdout (for debugging)
+	request.Write(os.Stdout)
+
+	var response string
+	path := request.URL.Path
+
+	switch {
+	case path == "/":
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	case path == "/user-agent":
+		userAgent := request.UserAgent()
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+	case strings.HasPrefix(path, "/echo/"):
+		echo := strings.TrimPrefix(path, "/echo/")
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
+	default:
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+	}
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
+	for {
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
-
-	defer conn.Close()
-
-	request, err := http.ReadRequest(bufio.NewReader(conn))
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
-	}
-
-	var response string
-	path := request.URL.Path
-
-	if path == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if path == "/user-agent" {
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(request.UserAgent()), request.UserAgent())
-	} else if strings.Contains(path, "/echo/") {
-		echo := path[6:]
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-
-	conn.Write([]byte(response))
-	conn.Close()
 }
